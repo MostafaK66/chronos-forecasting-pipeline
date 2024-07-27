29f020a36319f9dd5019f10dcf19bc33d90f74b0
MostafaK66 <mos.karimzadeh@gmail.com>
2024-07-27 13:04:08 +0200

    Minor cleaning on settings.py.

--- settings.py
+++ settings.py
@@ -3,14 +3,6 @@ from autogluon.common import space
 FILE_PATH = "input/medium_views_published_holidays.csv"
 PREDICTION_LENGTH = 100
 MODEL_SIZE = "chronos_tiny"
-#
-# CUSTOM_HYPERPARAMETERS = {
-#     "Chronos": {
-#         "model_path": "tiny",
-#         "batch_size": space.Int(8, 16),
-#         "learning_rate": space.Categorical(1e-3, 1e-4),
-#     }
-# }
 
 CUSTOM_HYPERPARAMETERS = {
     "Chronos": {
